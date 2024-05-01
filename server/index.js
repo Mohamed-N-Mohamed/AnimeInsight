c44554a1317e049938f5e6022a0f8b17bd1012e1
Mohamed Mohamed <mohamed.mohamed@bennamann.com>
2024-05-01 16:57:18 +0100

    Added connect to the database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import mysql from 'mysql';
 const app = express();
 const port = 3000;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'anime',
+});
+
+connection.connect();
+
+//query
+const query = 'Select * from questions';
+connection.query(query, (error, results, fields) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(results);
+});
 
 app.get('/', (req, res) => {
   res.send('this is working');
