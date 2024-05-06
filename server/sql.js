31c6a5c63efa5051b0468d8248217e1cbb2aec2a
Mohamed Mohamed <mohamed-n-mohamed@outlook.com>
2024-05-06 17:20:49 +0100

    testing github

--- server/sql.js
+++ server/sql.js
@@ -17,6 +17,7 @@ module.exports = {
         if (error) {
           console.log(error);
           res.status(500).send('Internal Server Error');
+          console.log('testing github');
           return;
         }
 
